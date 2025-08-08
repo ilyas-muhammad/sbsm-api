497ec9d1253693f3aba5568ecc7d58df5d93bc19
ilyas-muhammad <ell@petaniweb.com>
2025-08-08 16:49:18 +0700

    Add CORS middleware to FastAPI application for cross-origin requests

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from model import Measurement
 from spreadsheet import append_row, get_all_records
 
 app = FastAPI()
 
 load_dotenv()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def ping():
